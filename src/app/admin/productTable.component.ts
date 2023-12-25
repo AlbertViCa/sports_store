ffa8030284244377cd8e5d77ceffc78f763b239f
AlbertViCa <albert0-v.c@hotmail.com>
2023-12-25 10:54:42 -0600

    Product Table features implemented.

--- src/app/admin/productTable.component.ts
+++ src/app/admin/productTable.component.ts
@@ -1,10 +1,38 @@
-import {Component} from '@angular/core';
+import {Component, IterableDiffer, IterableDiffers, ViewChild} from '@angular/core';
+
+import { MatTableDataSource } from '@angular/material/table';
+import { Product } from '../model/product.model';
+import { ProductRepository } from '../model/product.repository';
+import { MatPaginator } from '@angular/material/paginator';
 
 @Component({
     selector: 'app-product-table',
-    template: `
-    <h3 style="padding: 20px;">
-        Product table Placeholder
-    </h3>`
+    templateUrl: 'productTable.component.html'
 })
-export class ProductTableComponent {}
+export class ProductTableComponent {
+    colsAndRows: string[] = ['id', 'name', 'category', 'price', 'buttons'];
+    dataSource = new MatTableDataSource<Product>(this.repository.products());
+    differ: IterableDiffer<Product>;
+
+    constructor(private repository: ProductRepository, private differs: IterableDiffers) {
+        this.differ = differs.find(this.repository.products()).create();
+    }
+
+    ngDoCheck() {
+        let changes = this.differ?.diff(this.repository.products());
+        if (changes) {
+            this.dataSource = new MatTableDataSource<Product>(this.repository.products());
+        }
+    }
+
+    deleteProduct(id: number) {
+        this.repository.deleteProduct(id);
+    }
+
+    @ViewChild(MatPaginator) paginator?: MatPaginator;
+    ngAfterViewInit() {
+        if (this.paginator){
+            this.dataSource.paginator = this.paginator!;
+        }
+    }
+}
